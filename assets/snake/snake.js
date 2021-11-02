c4dbd8a850271f65b5db36fde74cda05196e5154
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-11-02 19:03:42 +0000

    Adding move rules

--- assets/snake/snake.js
+++ assets/snake/snake.js
@@ -1,7 +1,38 @@
 window.addEventListener("load", () => {
     let points, snake, running, apple, move, nextMove;
-
+    const ctx = document.getElementsById('snake-canvas').getContext('2d');
+    
     setDefault();
+    addKeyDownEventListener();
+    setInterval(renderFrame, 100);
+
+    function renderFrame() {
+        if (running) {
+            if(nextMove.x !== -move.x || nextMove.y !== -move.y) {
+
+            }
+            snake.push({x: processBound(getHead().x + move.x), y: processBound(getHead().y + move.y)});
+        }
+
+        ctx.fillStyle= 'yellow';
+        ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+
+        ctx.fillStyle= 'black';
+        snake.forEach(square => ctx.fillRect(square.x *20, square.y *20, 18, 18));
+
+        ctx.fillStyle= 'red';
+        ctx.fillRect(apple.x *20, apple.y *20, 18, 18)
+    }
+
+    function processBound(number) {
+        if (number > 19) {
+            return 0;
+        } else if (number < 0) {
+            return 19;
+        }
+        return number;
+    }
+
 
     function setDefault() {
         running = false;
@@ -21,6 +52,28 @@ window.addEventListener("load", () => {
 
     function generateRandomNumber(max) {
         return Math.floor(Math.random() * (max + 1));
+    }
+
+    function addKeyDownEventListener() {
+        window.addEventListener('keydown', e => {
+            if (e.code.startsWith('Arrow')) {
+                e.preventDefault();
+                running = true;
+            }
+            switch(e.code) {
+                case 'ArrowLeft':
+                    nextMove = {x: -1, y: 0};
+                    break;
+                case 'ArrowRight':
+                    nextMove = {x: 1, y: 0};
+                    break;
+                case 'ArrowDown':
+                    nextMove = {x: 0, y: 1};
+                    break;
+                case 'ArrowUp':
+                    nextMove = {x: 0, y: -1};
+            }
+        });
     }
 
 });
